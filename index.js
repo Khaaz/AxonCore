a159e092aa635ada92691467267f8df0a2d61fc3
Khaazz <lbellec@live.com>
2018-08-04 19:25:20 +0200

    Changed global export in index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-export { default } from './src/AxonClient';
+export { default as AxonClient } from './src/AxonClient';
 
-// Core
+// Structure
+export { default as Base } from './src/Structures/Base'
 export { default as Module} from './src/Structures/Module';
 export { default as Command} from './src/Structures/Command';
 export { default as Event} from './src/Structures/EventF';
 
 // Utility
 export { default as Collection} from './src/Utility/Collection';
+export { default as AxonUtils} from './src/Utility/AxonUtils';
 export { default as Utils} from './src/Utility/Utils';
-export { default as Enums} from './src/Utility/Enums';
 export { default as Resolver} from './src/Utility/Resolver';
+export { default as Enums} from './src/Utility/Enums';
 
-// MongoDB
+// DataBase
+export { default as JsonService} from './src/Database/JsonService';
+export { default as MongoService} from './src/Database/MongoService';
+// Mongoose Models
 export { default as AxonSchema} from './src/Database/Models/AxonSchema';
 export { default as GuildSchema} from './src/Database/Models/GuildSchema';
+
+// Loggers
+export { default as DefLogger } from './src/Loggers/DefLogger'
+
+// Errors
+export { default as AxonError} from './src/Errors/AxonError';
+export { default as AxonCommandError} from './src/Errors/AxonCommandError';
