608e4356ebb257497e148d9ff2c10c59727ae76b
khaazz <khaaz.dev@gmail.com>
2018-11-27 19:15:45 +0100

    Mongoose connection, fix to depreciation warnings

--- example/src/index.js
+++ example/src/index.js
@@ -1,12 +1,21 @@
 'use strict';
+
 import Bot from './Bot';
 import customConf from './configs/customConf.json';
 
 if (customConf.db === 1) {
     try {
         const mongoose = require('mongoose');
-        mongoose.connect('mongodb://localhost/AxonCoreDB');
-        Bot.Logger.notice('Connected to AxonCore DataBase.');
+        mongoose.connect('mongodb://localhost/AxonCoreDB', {
+            useCreateIndex: true,
+            autoReconnect: true,
+        })
+            .then(() => {
+                Bot.Logger.notice('Connected to AxonCore DataBase.');
+            })
+            .catch(err => {
+                Bot.Logger.emerg('Could NOT connect to AxonCore DataBase.\n' + err.stack);
+            });
     } catch (e) {
         Bot.Logger.emerg('Could NOT connect to AxonCore DataBase.\n' + e.stack);
     }
