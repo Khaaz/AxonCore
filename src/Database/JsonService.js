a9ad7a946107d4ba06af2e52f61ba9e5c9923f06
Olybear9 <olybear9@gmail.com>
2018-12-05 16:05:59 -0700

    Update JsonService.js

--- src/Database/JsonService.js
+++ src/Database/JsonService.js
@@ -1,119 +1,129 @@
 'use strict';
-/* eslint-disable */
+import db from './JSON/functions';
+import { resolve } from 'url';
 class JsonService {
     /**
-     * Retrieves the axon schema from the DB.
+     * Retrieve Axon schema from DB (unique schema)
      *
      * @static
      * @returns {Promise<Object|null>} AxonSchema Object or null
      * @memberof MongoService
      */
     static fetchAxon() {
-        //
+        return db.read('guild', 1);
     }
 
     /**
-     * Retreives the Guild Schema for the specified guild.
+     * Retreive the Guild Schema for the specfici guidld
      *
      * @static
      * @param {String} gID - guild ID
-     * @returns {Promise<Object|null>} GuildSchema or null
+     * @returns {Promise<Object|null>} GuildSchema Object or null
      * @memberof MongoService
      */
     static fetchGuild(gID) {
-        //
+        return db.read(gID)
     }
 
     /**
-     * Initialises a default schema for Axon.
+     * Initialise the default schema for Axon with default value
      *
      * @static
-     * @returns {Promise<Object>} Newly created Axon Schema
+     * @returns {Promise<Object>} Axon Schema Object newly created
      * @memberof MongoService
      */
     static initAxon() {
-        //
-    }
-
-    static initGuild(gID) {
-        //
+        db.write('guild', {parent: '1'});
     }
 
     /**
-     * Updates the blacklisted users.
+     * Initialise the default schema for Axon with default value
      *
      * @static
-     * @param {Array<String>} blacklistedUsers - Array of blacklisted users
-     * @returns {Promise} Updated AxonSchema
+     * @param {String} gID - guild ID
+     * @returns {Promise<Object>} Guild Schema Object newly created
+     * @memberof MongoService
+     */
+    static initGuild(gID) {
+        return db.write('guild', {parent: gID});
+    }
+
+    /**
+     * Update the blacklisted users
+     *
+     * @static
+     * @param {Array<String>} blacklistedUsers - Array of blacklistedUsers
+     * @returns {Promise} AxonSchema updated
      * @memberof MongoService
      */
     static updateBlacklistUser(blacklistedUsers) {
-        //
+        return db.write('blacklist/users', blacklistedUsers);
     }
 
     /**
-     * Updates the blacklisted guilds.
+     * Update the blacklisted guilds
      *
      * @static
      * @param {Array<String>} blacklistedGuilds - Array of blacklistedUsers
-     * @returns {Promise} Updated AxonSchema
+     * @returns {Promise} AxonSchema updated
      * @memberof MongoService
      */
     static updateBlacklistGuild(blacklistedGuilds) {
-        //
+        return db.write('blacklist/guilds', blacklistedGuilds);
     }
 
     /**
-     * Updates the guild prefix array for the specified guild.
+     * Update the guild prefix array for that guild
      *
      * @static
      * @param {String} gID - guild ID
      * @param {Array<String>} prefixArr - Array of prefixes
-     * @returns {Promise} Updated GuildSchema
+     * @returns {Promise} GuilSchema updated
      * @memberof MongoService
      */
     static updateGuildPrefix(gID, prefixArr) {
-        //
+        return db.write('guild', {parent: gID, child: "prefix", value: prefixArr});
     }
 
     /**
-     * Updates the modules array for the specified guild.
+     * Update the guilds modules array for that guild
      *
      * @static
      * @param {Array<String>} modulesArr - Array of modules label
-     * @returns {Promise} Updated GuildSchema
+     * @returns {Promise} GuildSchema updated
      * @memberof MongoService
      */
     static updateModule(gID, modulesArr) {
-        //
+        return db.write('guild', {parent: gID, child: 'commands', value: modulesArr})
     }
 
     /**
-     * Updates commands array for the specified guild.
+     * Update the guilds commands array for that guild
      *
      * @static
      * @param {Array<String>} commandsArr - Array of commands label
-     * @returns {Promise} Updated GuildSchema
+     * @returns {Promise} GuildSchema updated
      * @memberof MongoService
      */
     static updateCommand(gID, commandsArr) {
-        //
+        return db.write('guild', {parent: gID, child: 'commands', value: commandsArr});
     }
 
     /**
-     * Updates the events array for the specified guild.
+     * Update the guilds events array for that guild
      *
      * @static
      * @param {Array<String>} eventsArr - Array of events label
-     * @returns {Promise} Updated GuildSchema
+     * @returns {Promise} GuildSchema updated
      * @memberof MongoService
      */
     static updateEvent(gID, eventsArr) {
-        //
+        return db.write('guild', {parent: gID, child: 'events', value: eventsArr});
     }
 
     /**
-     * Updates the given schema in the DB with a new schema.
+     * Update the given schema in the DB
+     * with value in current schema object
      *
      * @static
      * @param {Object} schema - the schema object to update
@@ -121,7 +131,7 @@ class JsonService {
      * @memberof MongoService
      */
     static saveSchema(schema) {
-        //
+        return db.write('guild', {parent: '1', value: eventsArr});
     }
 }
 
