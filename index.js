e451b536436a9a7e6a1f98d1c49e9c8c2f1fee4f
khaazz <khaaz.dev@gmail.com>
2019-01-12 20:49:02 +0100

    Export files renaming

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-export { default } from './src/AxonClient';
-
-export { default as AxonClient } from './src/AxonClient';
-
-// Structure
-export { default as Base } from './src/Structures/Base';
-export { default as Module } from './src/Structures/Module';
-export { default as Command } from './src/Structures/Command';
-export { default as Event } from './src/Structures/Event';
-
-// Utility
-export { default as Collection } from './src/Utility/Collection';
-export { default as AxonUtils } from './src/Utility/AxonUtils';
-export { default as Utils } from './src/Utility/Utils';
-export { default as Resolver } from './src/Utility/Resolver';
-export { default as Enums } from './src/Utility/Enums';
-
-// Errors
-export { default as AxonError } from './src/Errors/AxonError';
-export { default as AxonCommandError } from './src/Errors/AxonCommandError';
+// Set options as a parameter, environment variable, or rc file.
+require = require('esm')(module/* , options*/); // eslint-disable-line
+module.exports = require('./index.js');
