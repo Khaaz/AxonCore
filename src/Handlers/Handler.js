43841738abbc032681284f38cd84e99af77c5092
khaazz <khaaz.dev@gmail.com>
2019-08-03 16:57:38 +0200

    Abstract the Handler structure with its own class and implementation

---
+++ src/Handlers/Handler.js
@@ -0,0 +1,74 @@
+import NoAbstractInstanceException from '../Errors/NoAbstractInstanceException';
+import NotImplementedException from '../Errors/NotImplementedException';
+
+/**
+ * Abstract class for handlers.
+ * Events root handlers.
+ *
+ * @author KhaaZ
+ *
+ * @abstract
+ * @class Handler
+ */
+class Handler {
+    /**
+     * Creates an instance of Handler.
+     *
+     * @memberof Handler
+     */
+    constructor(axon, name, listeners) {
+        if (this.constructor === 'Handler') {
+            throw new NoAbstractInstanceException();
+        }
+
+        this._axon = axon;
+
+        this.name = name;
+        this._listeners = listeners;
+
+        this._handle = this._handle.bind(this);
+    }
+
+    get size() {
+        return this._listeners.length;
+    }
+
+    async _handle(...args) {
+        const guildID = this.handle(...args);
+
+        // Tries to resolve guildConf if there is a guild
+        const guildConfig = guildID ? await this._axon.guildConfigs.getOrFetch(guildID) : null;
+
+        // Ignore blacklisted guilds
+        if (guildID && this._axon.axonConfig.isBlacklistedGuild(guildID) ) {
+            return;
+        }
+        for (const listener of this._listeners) {
+            if (!listener.load || !listener.module.enabled || !listener.enabled) { // globally disabled
+                continue;
+            }
+            // Ignore guild disabled Module/Event
+            if (guildConfig) {
+                if ( (guildConfig.isModuleDisabled(listener.module) && !listener.module.serverBypass)
+                    || (guildConfig.isListenerDisabled(listener) && !listener.serverBypass) ) {
+                    continue;
+                }
+            }
+            this._axon._execListener(listener, guildConfig, ...args);
+        }
+    }
+
+    /**
+     * Takesthe event parameters as arguments and returns the guild ID if possible or null.
+     *
+     * @param {Object} args - All parameters for this event
+     * @returns {String|null} The guild ID
+     *
+     * @memberof Handler
+     */
+    handle(...args) { // eslint-disable-line no-unused-vars
+        throw new NotImplementedException();
+    }
+}
+
+export default Handler;
--- src/Structures/EventManager.js
+++ src/Structures/EventManager.js
@@ -1,6 +1,9 @@
 import Base from './Base';
 
 import Collection from '../Utility/Collection';
+import Handler from '../Handlers/Handler';
+
+import * as HANDLERS from '../Handlers/eris/index';
 
 /**
  * Event Manager class
@@ -25,8 +28,10 @@ class EventManager extends Base {
         // For each eventName => Array of Event Obj
         this._listeners = {};
         // For each eventName => One Function
-        this._handlers = new Collection();
+        this._handlers = new Collection(Handler);
     }
+
+    // **** GETTERS **** //
 
     get events() {
         return this._handlers;
@@ -43,9 +48,13 @@ class EventManager extends Base {
         return this._listeners[eventName];
     }
 
+    // **** BINDERS **** //
+    /** Called by AxonClient */
+
     /**
      * Bind all listeners to a handler.
      * Create and register a handler for each event.
+     * Called by AxonClient in start method.
      * If the bot is ready, also call bindHandlers()
      *
      * @memberof EventManager
@@ -53,10 +62,6 @@ class EventManager extends Base {
     bindListeners() {
         // Create handler for each event
         for (const event in this._listeners) {
-            /** Doesn't register the handler if all listeners shouldn't be loaded */
-            if (this._listeners[event].every(e => !e.load) ) {
-                break;
-            }
             this.registerHandler(event);
         }
         // Bind Handlers to the event emission if bot is ready
@@ -73,29 +78,37 @@ class EventManager extends Base {
     bindHandlers() {
         // bind handler to event emission
         for (const [event, handler] of this._handlers) {
-            this.bot.on(event, handler.run);
-            this.Logger._initEvent(true, handler);
+            this.bot.on(event, handler._handle);
+            this.logger._initEvent(true, handler);
         }
     }
 
+    // **** REGISTERERS **** //
+
     /**
-     * Register a listener for the given event.
-     * Add the event object in the array of event objects for each event.
+     * Register a listener for the given discord event.
+     * Add the Listener in the array of Listener for each discord event.
+     * Called by ModuleLoader when registering an event.
      *
-     * @param {Object<Event>} event - Event Object
+     * @param {Object<Listener>} listener - Event Object
+     *
      * @memberof EventManager
      */
-    registerListener(event) {
-        this._listeners[event.eventName] = this._listeners[event.eventName] || [];
+    registerListener(listener) {
+        /** Doesn't load the listener */
+        if (!listener.load) {
+            return null;
+        }
+        this._listeners[listener.eventName] = this._listeners[listener.eventName] || [];
         // Remove/replace if already existing
-        const index = this._listeners[event.eventName].findIndex(l => l.label === event.label);
+        const index = this._listeners[listener.eventName].findIndex(l => l.label === listener.label);
         if (index > -1) {
-            this._listeners[event.eventName].splice(index, 1);
+            this._listeners[listener.eventName].splice(index, 1);
         }
         // Add Listener
-        this._listeners[event.eventName].push(event);
-        this.Logger._initEvent(false, event);
-        return this._listeners[event.eventName];
+        this._listeners[listener.eventName].push(listener);
+        this.logger._initEvent(false, listener);
+        return this._listeners[listener.eventName];
     }
 
     /**
@@ -105,130 +118,39 @@ class EventManager extends Base {
      *
      * @param {String} event - The Event name
      * @returns {Object} The new Handler created
+     *
      * @memberof EventManager
      */
     registerHandler(event) {
         let handler = this._handlers.get(event);
         if (handler) {
             /** Remove the current event if any registered */
-            this.bot.off(event, handler.run);
+            this.bot.off(event, handler._handle);
         }
-        handler = {};
-        handler.name = event;
-        handler.size = this._listeners[event].length;
-        handler.run = this.createHandler.bind(this, this._listeners[event] );
+        handler = new HANDLERS[event](this.axon, event, this._listeners[event] );
+
         this._handlers.set(event, handler);
 
         return handler;
     }
 
     /**
-     * Register an event.
+     * Register an event handler and start listen to this event.
      * Recreate a handler and bind it to the event emitter.
      *
-     * @param {String} event - The Event name to register
+     * @param {String} event - The Discord event name to register
      * @returns {Object} The Handler Object
+     *
      * @memberof EventManager
      */
     registerEvent(event) {
         const handler = this.registerHandler(event);
-        this.bot.on(event, handler.run);
+        this.bot.on(event, handler._handle);
         return handler;
     }
 
-    /**
-     * Create a handler function from all listeners.
-     * Checks if the module/event is globally disabled.
-     *
-     * @param {Array} events - All listeners for this event
-     * @param {Array} args - All arguments possibly passed to an event
-     * @memberof EventManager
-     */
-    createHandler(events, ...args) {
-        this._rootHandler(...args)
-            .then(gConf => {
-                for (const event of events) {
-                    if (!event.load || !event.module.enabled || !event.enabled) { // globally disabled
-                        continue;
-                    }
-                    // Ignore guild disabled Module/Event
-                    if (gConf) {
-                        if ( (this.AxonUtils.isModuleDisabled(event.module, gConf) && !this.module.serverBypass)
-                            || (this._isEventDisabled(event.label, gConf) && !this.serverBypass) ) {
-                            continue;
-                        }
-                    }
-                    event._execute(gConf, ...args)
-                        .then( () => {
-                            if (this.axon.params.debugMode) {
-                                this.Logger.verbose(`[EVENT](${event.eventName}) - ${event.label}`);
-                            }
-                        } )
-                        .catch(err => {
-                            this.Logger.error(`[EVENT](${event.eventName}) - ${event.label}\n${err}`);
-                        } );
-                }
-            } )
-            .catch(null);
-    }
-
-    /**
-     * Root function for all events.
-     * Try to resolve the guild object and guildConfig.
-     * If needed, test if the user is a bot or the guild is blacklisted and stop the event.
-     * Check if the event is disabled in the given guild or not.
-     *
-     * @param {Array} args - All arguments possibly passed to an event
-     * @returns {Promise<GuildConf?>}
-     * @memberof EventManager
-     */
-    _rootHandler(...args) {
-        return new Promise(async(resolve, reject) => {
-            // Tries to resolve a guild
-            let guild = null;
-            if (args[0] ) {
-                if (args[0].guild) { // member or channel
-                    if (args[0].user && args[0].user.bot) { // member | check not a bot
-                        return;
-                    }
-                    // eslint-disable-next-line prefer-destructuring
-                    guild = args[0].guild;
-                } else if (args[0].channel) { // message
-                    if (args[0].author && args[0].author.bot) { // message | check not a bot
-                        return;
-                    }
-                    // eslint-disable-next-line prefer-destructuring
-                    guild = args[0].channel.guild;
-                } else if (args[0].ownerID) { // guild
-                    guild = args[0];
-                }
-            }
-
-            // Tries to resolve guildConf if there is a guild
-            const guildConf = guild ? await this.axon.getGuildConf(guild.id) : null;
-
-            // Ignore blacklisted guilds
-            if (guild) {
-                if (this.axon.blacklistedGuilds.has(guild.id) ) {
-                    reject();
-                }
-            }
-            resolve(guildConf);
-        } );
-    }
-
-    /**
-     * Check if the event is disabled on the given guild
-     *
-     * @param {String} label - The event label
-     * @param {Object} guildConf - The guild Config object
-     * @returns {Boolean} True if disabled / Undefined if not
-     * @memberof Event
-     */
-    _isEventDisabled(label, guildConf) {
-        return guildConf.events.find(e => e === label);
-    }
-
+    // **** UNREGISTERERS **** //
+    
     /**
      * Unregister a listener.
      * Recreate the handler and listen to the updated handler
@@ -254,7 +176,7 @@ class EventManager extends Base {
             return false;
         }
         this.registerEvent(event);
-        this.Logger.info(`Event: Listener ${label} for ${event} unregistered!`);
+        this.logger.info(`Event: Listener ${label} for ${event} unregistered!`);
         return true;
     }
 
@@ -288,7 +210,7 @@ class EventManager extends Base {
             return false;
         }
         this.bot.off(event, handler.run);
-        this.Logger.info(`Event: ${event} unregistered!`);
+        this.logger.info(`Event: ${event} unregistered!`);
         return true;
     }
 }
