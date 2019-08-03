1841a6b7726915084a7fdbb42b1e6398f8fef7dd
khaazz <khaaz.dev@gmail.com>
2019-08-03 17:06:26 +0200

    CommandResponse used for consistent returns after a command execution

---
+++ src/Structures/Command/CommandResponse.js
@@ -0,0 +1,61 @@
+/**
+ * Build a Command Response - the formatted object used internally by the framework to resolve context.
+ *
+ * @class CommandResponse
+ *
+ * @prop {Boolean} success
+ * @prop {Boolean} triggerCooldown
+ */
+class CommandResponse {
+    /**
+     * Creates an instance of CommandResponse.
+     * Build the commandresponse from all options given in parameters
+     *
+     * @param {Object} [data={}]
+     * @param {Boolean} [data.success=true]
+     * @param {Boolean} [data.triggerCooldown=true]
+     * @param {Object|String} [data.error=null]
+     *
+     * @memberof CommandResponse
+     */
+    constructor(data = {} ) {
+        this.success = data.success !== undefined ? data.success : true;
+        this.triggerCooldown = data.triggerCooldown !== undefined ? data.triggerCooldown : true;
+        this.error = data.error || null;
+    }
+
+    /**
+     * By default returns the Command Response asynchronously.
+     *
+     * @returns {Promise<CommandResponse>}
+     *
+     * @memberof CommandResponse
+     */
+    resolve() {
+        return this.resolveAsync();
+    }
+
+    /**
+     * Returns the Command Response in a Promise (asynchronously)
+     *
+     * @returns {Promise<CommandResponse>}
+     *
+     * @memberof CommandResponse
+     */
+    resolveAsync() {
+        return Promise.resolve(this);
+    }
+
+    /**
+     * Returns the Command Response (synchronously)
+     *
+     * @returns {CommandResponse}
+     *
+     * @memberof CommandResponse
+     */
+    resolveSync() {
+        return this;
+    }
+}
+
+export default CommandResponse;
