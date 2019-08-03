0e9e33b6bd148c0f024794ff4ff9ebf462690283
khaazz <khaaz.dev@gmail.com>
2019-08-03 17:02:08 +0200

    Rename Models to Mongo (for extension to other dbs)+ cleanup

---
+++ src/Database/Mongo/GuildSchema.js
@@ -0,0 +1,41 @@
+import { Schema, model } from 'mongoose';
+
+const guildSchema = new Schema( {
+    /** Index */
+    guildID: { type: String, required: true, index: true }, // GUILD ID
+
+    prefixes: { type: Array, default: [], required: true },
+
+    /**
+     * Core
+     */
+    modules: { type: Array, default: [] }, // Array of disabled modules (labels)
+    commands: { type: Array, default: [] }, // Array of disabled commands (labels)
+    eventListeners: { type: Array, default: [] }, // Array of disabled listeners (labels)
+
+    /**
+     * General infos
+     */
+    createdAt: { type: Date, default: Date.now }, // date of schema creation
+    updatedAt: { type: Date, default: Date.now }, // data of last DB update
+
+    /**
+     * Ignore
+     */
+    ignoredUsers: { type: Array, default: [] }, // ids
+    ignoredRoles: { type: Array, default: [] }, // ids
+    ignoredChannels: { type: Array, default: [] }, // ids
+
+    /**
+     * Moderation
+     */
+    modOnly: { type: Boolean, default: false },
+    modRoles: { type: Array, default: [] },
+    modUsers: { type: Array, default: [] },
+
+}, {
+    autoIndex: true,
+    minimize: false,
+} );
+
+export default model('Guild', guildSchema);
