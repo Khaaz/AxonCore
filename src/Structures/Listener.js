2d8d1a29dc7f0fcb83c49c5529066ac40dd40836
khaazz <khaaz.dev@gmail.com>
2019-08-03 16:56:21 +0200

    Rename event to Listener (consistency)

--- src/Structures/Listener.js
+++ src/Structures/Listener.js
@@ -1,47 +1,41 @@
 import Base from './Base';
 
 /**
- * Default Event class
- * Extends Event to create an event
+ * Default Listener class
+ * Extends Listener to create an event
  *
  * @author KhaaZ
  *
- * @class Event
+ * @class Listener
  * @extends {Base}
  */
-class Event extends Base {
+class Listener extends Base {
     /**
-     * Creates an Event instance.
+     * Creates an Listener instance.
      * @param {Object<Module>} module
-     *
-     * @prop {Object<AxonClient>} axon - Axon Client [GETTER: _axon]
-     * @prop {Object<Eris.Client>} bot - Eris bot Client [GETTER: _axon.client]
-     * @prop {Object} Logger - Logger Object/Methods [GETTER: axon.Logger]
-     * @prop {Object} AxonUtils - AxonUtils Object/Methods [GETTER: axon.AxonUtils]
-     * @prop {Object} Utils - Utils Object/Methods [GETTER: axon.Utils]
      *
      * @prop {Object<Module>} module - Module object [GETTER: _module]
      * @prop {String} eventName - The discord event name
-     * @prop {String} label - The function name
+     * @prop {String} label - The listener name
      *
      * @prop {Boolean} load - Whether to load this event on startup or not
      * @prop {Boolean} [enabled=module.enabled] - Whether the event is enabled or not
      * @prop {Boolean} [serverBypass=module.serverBypass] - Can the event be disabled?
      *
      * @prop {Object} infos - Default infos about the event
-     * @prop {Array} infos.owners - Event owners/authors
-     * @prop {String} infos.description - Event description
+     * @prop {Array} infos.owners - Listener owners/authors
+     * @prop {String} infos.description - Listener description
      *
-     * @memberof Event
+     * @memberof Listener
      */
     constructor(module) {
         super(module.axon);
 
         this._module = module;
 
-        /** Event Name (Discord name) */
+        /** Listener Name (Discord name) */
         this.eventName = 'event';
-        /** Event name (Function name) */
+        /** Listener name (Function name) */
         this.label = 'label';
 
         this.load = true;
@@ -62,14 +56,15 @@ class Event extends Base {
     /**
      * Promisify the return execute return to prevent promise issue
      *
-     * @param {Object?} guildConf - the guildConfig or undefined if not a guild event
+     * @param {Object?} guildConfig - the guildConfig or undefined if not a guild event
      * @param {Array} args - Array of the events arguments
      * @returns {Promise}
-     * @memberof Event
+     *
+     * @memberof Listener
      */
-    async _execute(guildConf, ...args) {
-        return this.execute(...args, guildConf);
+    async _execute(guildConfig, ...args) {
+        return this.execute(...args, guildConfig);
     }
 }
 
-export default Event;
+export default Listener;
