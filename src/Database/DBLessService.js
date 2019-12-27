02c3a43982847f8d6083a1d0bd7eb22ca4259be4
Null <lolmaster203@gmail.com>
2019-12-26 21:48:22 -0600

    Added in-memory usage

--- src/Database/DBLessService.js
+++ src/Database/DBLessService.js
@@ -22,52 +22,78 @@ class DDBLessService extends DBService {
 
     }
 
-    fetchAxon() {
-        return Promise.resolve(new AxonConfig(this.axon, {} ) );
+    async fetchAxon() {
+        let axon = this.axon.axonConfig;
+        if (!axon) {
+            axon = await this.initAxon();
+        }
+        return Promise.resolve(axon);
     }
 
-    fetchGuild(gID) {
-        return Promise.resolve(new GuildConfig(this.axon, { guildID: gID } ) );
+    async fetchGuild(gID) {
+        let guild = this.axon.guildConfigs.get(gID);
+        if (!guild) {
+            guild = await this.initGuild(gID);
+        }
+        return guild;
     }
 
     initAxon() {
-        return Promise.resolve(new AxonConfig(this.axon, {} ) );
+        this.axon.axonConfig = new AxonConfig(this.axon, {} );
+        return Promise.resolve(this.axon.axonConfig);
     }
 
     initGuild(gID) {
         return Promise.resolve(new GuildConfig(this.axon, { guildID: gID } ) );
     }
 
-    updateBlacklistUser(blacklistedUsers) {
-        return Promise.resolve(new AxonConfig(this.axon, { bannedUsers: blacklistedUsers } ) );
+    async updateBlacklistUser(blacklistedUsers) {
+        return this.updateAxon('bannedUsers', blacklistedUsers);
     }
 
-    updateBlacklistGuild(blacklistedGuilds) {
-        return Promise.resolve(new AxonConfig(this.axon, { bannedGuilds: blacklistedGuilds } ) );
+    async updateBlacklistGuild(blacklistedGuilds) {
+        return this.updateAxon('bannedGuilds', blacklistedGuilds);
     }
 
-    updateGuildPrefix(gID, prefixArr) {
-        return Promise.resolve(new GuildConfig(this.axon, { guildID: gID, prefixes: prefixArr } ) );
+    async updateGuildPrefix(gID, prefixArr) {
+        return this.updateGuild('prefixes', gID, prefixArr);
     }
 
     updateModule(gID, modulesArr) {
-        return Promise.resolve(new GuildConfig(this.axon, { guildID: gID, modules: modulesArr } ) );
+        return this.updateGuild('modules', gID, modulesArr);
     }
 
     updateCommand(gID, commandArr) {
-        return Promise.resolve(new GuildConfig(this.axon, { guildID: gID, commands: commandArr } ) );
+        return this.updateGuild('commands', gID, commandArr);
     }
 
-    updateEvent(gID, eventArr) {
-        return Promise.resolve(new GuildConfig(this.axon, { guildID: gID, listeners: eventArr } ) );
+    async updateEvent(gID, eventArr) {
+        return this.updateGuild('listeners', gID, eventArr);
     }
 
     saveAxonSchema(axonSchema) {
-        return Promise.resolve(new AxonConfig(this.axon, axonSchema) );
+        this.axon.axonConfig = axonSchema;
+        return axonSchema;
     }
 
     saveGuildSchema(gID, guildSchema) {
-        return Promise.resolve(new GuildConfig(this.axon, guildSchema) );
+        return this.axon.guildConfigs.set(gID, guildSchema);
+    }
+
+    async updateGuild(key, gID, value) {
+        const guild = await this.fetchGuild(gID);
+        guild[key] = value;
+        this.saveGuildSchema(gID, guild);
+        return guild;
+    }
+
+    async updateAxon(key, value) {
+        let axonConf = this.axon.axonConfig;
+        if (!axonConf) {
+            axonConf = await this.initAxon();
+        }
+        axonConf[key] = value;
+        return axonConf;
     }
 }
 
