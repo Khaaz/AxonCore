1381379edd7db3d3618492ba397bae5ce1307ace
Khaazz <khaaz.dev@gmail.com>
2020-01-16 19:23:29 +0100

    Adds back Resolver definition to global export

--- esmExport.js
+++ esmExport.js
@@ -23,6 +23,7 @@ export { default as GuildConfig } from './src/Structures/DataStructure/GuildConf
 export { default as Collection } from './src/Utility/Collection';
 export { default as Utils } from './src/Utility/Utils';
 
+export { default as Resolver } from './src/Libraries/definitions/Resolver';
 export { default as Embed } from './src/Utility/External/Embed';
 export { default as Prompt } from './src/Utility/External/Prompt';
 export { default as MessageCollector } from './src/Utility/External/MessageCollector';
