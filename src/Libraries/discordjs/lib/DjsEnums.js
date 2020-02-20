baa2eded5da793233155b4dffc06dc7ac05ecccc
Bsian <bsian@staff.libraryofcode.us>
2020-02-20 13:39:04 +0000

    Enum documentation fixes

--- src/Libraries/discordjs/lib/DjsEnums.js
+++ src/Libraries/discordjs/lib/DjsEnums.js
@@ -1,53 +1,54 @@
 /**
- * @typedef {String} EVENTS
- * * RATE_LIMIT: 'rateLimit'
- * * READY: 'ready'
- * * RESUME: 'resume'
- * * GUILD_CREATE: 'guildCreate'
- * * GUILD_DELETE: 'guildDelete'
- * * GUILD_UPDATE: 'guildUpdate'
- * * GUILD_UNAVAILABLE: 'guildUnavailable'
- * * GUILD_AVAILABLE: 'guildAvailable'
- * * GUILD_MEMBER_ADD: 'guildMemberAdd'
- * * GUILD_MEMBER_REMOVE: 'guildMemberRemove'
- * * GUILD_MEMBER_UPDATE: 'guildMemberUpdate'
- * * GUILD_MEMBER_AVAILABLE: 'guildMemberAvailable'
- * * GUILD_MEMBER_SPEAKING: 'guildMemberSpeaking'
- * * GUILD_MEMBERS_CHUNK: 'guildMembersChunk'
- * * GUILD_INTEGRATIONS_UPDATE: 'guildIntegrationsUpdate'
- * * GUILD_ROLE_CREATE: 'roleCreate'
- * * GUILD_ROLE_DELETE: 'roleDelete'
- * * GUILD_ROLE_UPDATE: 'roleUpdate'
- * * GUILD_EMOJI_CREATE: 'emojiCreate'
- * * GUILD_EMOJI_DELETE: 'emojiDelete'
- * * GUILD_EMOJI_UPDATE: 'emojiUpdate'
- * * GUILD_BAN_ADD: 'guildBanAdd'
- * * GUILD_BAN_REMOVE: 'guildBanRemove'
- * * CHANNEL_CREATE: 'channelCreate'
- * * CHANNEL_DELETE: 'channelDelete'
- * * CHANNEL_UPDATE: 'channelUpdate'
- * * CHANNEL_PINS_UPDATE: 'channelPinsUpdate'
- * * MESSAGE_CREATE: 'message'
- * * MESSAGE_DELETE: 'messageDelete'
- * * MESSAGE_UPDATE: 'messageUpdate'
- * * MESSAGE_BULK_DELETE: 'messageDeleteBulk'
- * * MESSAGE_REACTION_ADD: 'messageReactionAdd'
- * * MESSAGE_REACTION_REMOVE: 'messageReactionRemove'
- * * MESSAGE_REACTION_REMOVE_ALL: 'messageReactionRemoveAll'
- * * USER_UPDATE: 'userUpdate'
- * * USER_NOTE_UPDATE: 'userNoteUpdate'
- * * USER_SETTINGS_UPDATE: 'clientUserSettingsUpdate'
- * * USER_GUILD_SETTINGS_UPDATE: 'clientUserGuildSettingsUpdate'
- * * PRESENCE_UPDATE: 'presenceUpdate'
- * * VOICE_STATE_UPDATE: 'voiceStateUpdate'
- * * TYPING_START: 'typingStart'
- * * TYPING_STOP: 'typingStop'
- * * WEBHOOKS_UPDATE: 'webhookUpdate'
- * * DISCONNECT: 'disconnect'
- * * RECONNECTING: 'reconnecting'
- * * ERROR: 'error'
- * * WARN: 'warn'
- * * DEBUG: 'debug'
+ * @type {{
+ * RATE_LIMIT: 'rateLimit'
+ * READY: 'ready'
+ * RESUME: 'resume'
+ * GUILD_CREATE: 'guildCreate'
+ * GUILD_DELETE: 'guildDelete'
+ * GUILD_UPDATE: 'guildUpdate'
+ * GUILD_UNAVAILABLE: 'guildUnavailable'
+ * GUILD_AVAILABLE: 'guildAvailable'
+ * GUILD_MEMBER_ADD: 'guildMemberAdd'
+ * GUILD_MEMBER_REMOVE: 'guildMemberRemove'
+ * GUILD_MEMBER_UPDATE: 'guildMemberUpdate'
+ * GUILD_MEMBER_AVAILABLE: 'guildMemberAvailable'
+ * GUILD_MEMBER_SPEAKING: 'guildMemberSpeaking'
+ * GUILD_MEMBERS_CHUNK: 'guildMembersChunk'
+ * GUILD_INTEGRATIONS_UPDATE: 'guildIntegrationsUpdate'
+ * GUILD_ROLE_CREATE: 'roleCreate'
+ * GUILD_ROLE_DELETE: 'roleDelete'
+ * GUILD_ROLE_UPDATE: 'roleUpdate'
+ * GUILD_EMOJI_CREATE: 'emojiCreate'
+ * GUILD_EMOJI_DELETE: 'emojiDelete'
+ * GUILD_EMOJI_UPDATE: 'emojiUpdate'
+ * GUILD_BAN_ADD: 'guildBanAdd'
+ * GUILD_BAN_REMOVE: 'guildBanRemove'
+ * CHANNEL_CREATE: 'channelCreate'
+ * CHANNEL_DELETE: 'channelDelete'
+ * CHANNEL_UPDATE: 'channelUpdate'
+ * CHANNEL_PINS_UPDATE: 'channelPinsUpdate'
+ * MESSAGE_CREATE: 'message'
+ * MESSAGE_DELETE: 'messageDelete'
+ * MESSAGE_UPDATE: 'messageUpdate'
+ * MESSAGE_BULK_DELETE: 'messageDeleteBulk'
+ * MESSAGE_REACTION_ADD: 'messageReactionAdd'
+ * MESSAGE_REACTION_REMOVE: 'messageReactionRemove'
+ * MESSAGE_REACTION_REMOVE_ALL: 'messageReactionRemoveAll'
+ * USER_UPDATE: 'userUpdate'
+ * USER_NOTE_UPDATE: 'userNoteUpdate'
+ * USER_SETTINGS_UPDATE: 'clientUserSettingsUpdate'
+ * USER_GUILD_SETTINGS_UPDATE: 'clientUserGuildSettingsUpdate'
+ * PRESENCE_UPDATE: 'presenceUpdate'
+ * VOICE_STATE_UPDATE: 'voiceStateUpdate'
+ * TYPING_START: 'typingStart'
+ * TYPING_STOP: 'typingStop'
+ * WEBHOOKS_UPDATE: 'webhookUpdate'
+ * DISCONNECT: 'disconnect'
+ * RECONNECTING: 'reconnecting'
+ * ERROR: 'error'
+ * WARN: 'warn'
+ * DEBUG: 'debug'
+ * }}
  * @enum {String}
  */
 export const EVENTS = {
@@ -102,37 +103,39 @@ export const EVENTS = {
 };
 
 /**
- * @typedef {String} DISCORD_LIB_PERMISSIONS
- * * CREATE_INSTANT_INVITE: 'CREATE_INSTANT_INVITE'
- * * KICK_MEMBERS: 'KICK_MEMBERS'
- * * BAN_MEMBERS: 'BAN_MEMBERS'
- * * ADMINISTRATOR: 'ADMINISTRATOR'
- * * MANAGE_CHANNELS: 'MANAGE_CHANNELS'
- * * MANAGE_GUILD: 'MANAGE_GUILD'
- * * ADD_REACTIONS: 'ADD_REACTIONS'
- * * VIEW_AUDIT_LOG: 'VIEW_AUDIT_LOG'
- * * PRIORITY_SPEAKER: 'PRIORITY_SPEAKER'
- * * STREAM: 'STREAM'
- * * VIEW_CHANNEL: 'VIEW_CHANNEL'
- * * SEND_MESSAGES: 'SEND_MESSAGES'
- * * SEND_TTS_MESSAGES: 'SEND_TTS_MESSAGES'
- * * MANAGE_MESSAGES: 'MANAGE_MESSAGES'
- * * EMBED_LINKS: 'EMBED_LINKS'
- * * ATTACH_FILES: 'ATTACH_FILES'
- * * READ_MESSAGE_HISTORY: 'READ_MESSAGE_HISTORY'
- * * MENTION_EVERYONE: 'MENTION_EVERYONE'
- * * USE_EXTERNAL_EMOJIS: 'USE_EXTERNAL_EMOJIS'
- * * CONNECT: 'CONNECT'
- * * SPEAK: 'SPEAK'
- * * MUTE_MEMBERS: 'MUTE_MEMBERS'
- * * DEAFEN_MEMBERS: 'DEAFEN_MEMBERS'
- * * MOVE_MEMBERS: 'MOVE_MEMBERS'
- * * USE_VAD: 'USE_VAD'
- * * CHANGE_NICKNAME: 'CHANGE_NICKNAME'
- * * MANAGE_NICKNAMES: 'MANAGE_NICKNAMES'
- * * MANAGE_ROLES: 'MANAGE_ROLES'
- * * MANAGE_WEBHOOKS: 'MANAGE_WEBHOOKS'
- * * MANAGE_EMOJIS: 'MANAGE_EMOJIS'
+ * @type {{
+ * CREATE_INSTANT_INVITE: 'CREATE_INSTANT_INVITE',
+ * KICK_MEMBERS: 'KICK_MEMBERS',
+ * BAN_MEMBERS: 'BAN_MEMBERS',
+ * ADMINISTRATOR: 'ADMINISTRATOR',
+ * MANAGE_CHANNELS: 'MANAGE_CHANNELS',
+ * MANAGE_GUILD: 'MANAGE_GUILD',
+ * ADD_REACTIONS: 'ADD_REACTIONS',
+ * VIEW_AUDIT_LOG: 'VIEW_AUDIT_LOG',
+ * PRIORITY_SPEAKER: 'PRIORITY_SPEAKER',
+ * STREAM: 'STREAM',
+ * VIEW_CHANNEL: 'VIEW_CHANNEL',
+ * SEND_MESSAGES: 'SEND_MESSAGES',
+ * SEND_TTS_MESSAGES: 'SEND_TTS_MESSAGES',
+ * MANAGE_MESSAGES: 'MANAGE_MESSAGES',
+ * EMBED_LINKS: 'EMBED_LINKS',
+ * ATTACH_FILES: 'ATTACH_FILES',
+ * READ_MESSAGE_HISTORY: 'READ_MESSAGE_HISTORY',
+ * MENTION_EVERYONE: 'MENTION_EVERYONE',
+ * USE_EXTERNAL_EMOJIS: 'USE_EXTERNAL_EMOJIS',
+ * VIEW_GUILD_ANALYTICS: 'VIEW_GUILD_ANALYTICS',
+ * CONNECT: 'CONNECT',
+ * SPEAK: 'SPEAK',
+ * MUTE_MEMBERS: 'MUTE_MEMBERS',
+ * DEAFEN_MEMBERS: 'DEAFEN_MEMBERS',
+ * MOVE_MEMBERS: 'MOVE_MEMBERS',
+ * USE_VAD: 'USE_VAD',
+ * CHANGE_NICKNAME: 'CHANGE_NICKNAME',
+ * MANAGE_NICKNAMES: 'MANAGE_NICKNAMES',
+ * MANAGE_ROLES: 'MANAGE_ROLES',
+ * MANAGE_WEBHOOKS: 'MANAGE_WEBHOOKS',
+ * MANAGE_EMOJIS: 'MANAGE_EMOJIS',
+ * }}
  * @enum {String}
  */
 export const DISCORD_LIB_PERMISSIONS = {
@@ -155,6 +158,7 @@ export const DISCORD_LIB_PERMISSIONS = {
     READ_MESSAGE_HISTORY: 'READ_MESSAGE_HISTORY',
     MENTION_EVERYONE: 'MENTION_EVERYONE',
     USE_EXTERNAL_EMOJIS: 'USE_EXTERNAL_EMOJIS',
+    VIEW_GUILD_ANALYTICS: 'VIEW_GUILD_ANALYTICS',
     CONNECT: 'CONNECT',
     SPEAK: 'SPEAK',
     MUTE_MEMBERS: 'MUTE_MEMBERS',
@@ -167,39 +171,41 @@ export const DISCORD_LIB_PERMISSIONS = {
     MANAGE_WEBHOOKS: 'MANAGE_WEBHOOKS',
     MANAGE_EMOJIS: 'MANAGE_EMOJIS',
 };
-
+   
 /**
- * @typedef {Array<String>} PERMISSIONS
- * * 'CREATE_INSTANT_INVITE'
- * * 'KICK_MEMBERS'
- * * 'BAN_MEMBERS'
- * * 'ADMINISTRATOR'
- * * 'MANAGE_CHANNELS'
- * * 'MANAGE_GUILD'
- * * 'ADD_REACTIONS'
- * * 'VIEW_AUDIT_LOG'
- * * 'PRIORITY_SPEAKER'
- * * 'STREAM'
- * * 'VIEW_CHANNEL'
- * * 'SEND_MESSAGES'
- * * 'SEND_TTS_MESSAGES'
- * * 'MANAGE_MESSAGES'
- * * 'EMBED_LINKS'
- * * 'ATTACH_FILES'
- * * 'READ_MESSAGE_HISTORY'
- * * 'MENTION_EVERYONE'
- * * 'USE_EXTERNAL_EMOJIS'
- * * 'CONNECT'
- * * 'SPEAK'
- * * 'MUTE_MEMBERS'
- * * 'DEAFEN_MEMBERS'
- * * 'MOVE_MEMBERS'
- * * 'USE_VAD'
- * * 'CHANGE_NICKNAME'
- * * 'MANAGE_NICKNAMES'
- * * 'MANAGE_ROLES'
- * * 'MANAGE_WEBHOOKS'
- * * 'MANAGE_EMOJIS'
+ * @type {[
+    'CREATE_INSTANT_INVITE',
+    'KICK_MEMBERS',
+    'BAN_MEMBERS',
+    'ADMINISTRATOR',
+    'MANAGE_CHANNELS',
+    'MANAGE_GUILD',
+    'ADD_REACTIONS',
+    'VIEW_AUDIT_LOG',
+    'PRIORITY_SPEAKER',
+    'STREAM',
+    'VIEW_CHANNEL',
+    'SEND_MESSAGES',
+    'SEND_TTS_MESSAGES',
+    'MANAGE_MESSAGES',
+    'EMBED_LINKS',
+    'ATTACH_FILES',
+    'READ_MESSAGE_HISTORY',
+    'MENTION_EVERYONE',
+    'USE_EXTERNAL_EMOJIS',
+    'VIEW_GUILD_ANALYTICS',
+    'CONNECT',
+    'SPEAK',
+    'MUTE_MEMBERS',
+    'DEAFEN_MEMBERS',
+    'MOVE_MEMBERS',
+    'USE_VAD',
+    'CHANGE_NICKNAME',
+    'MANAGE_NICKNAMES',
+    'MANAGE_ROLES',
+    'MANAGE_WEBHOOKS',
+    'MANAGE_EMOJIS',
+    ]}
  */
 export const PERMISSIONS = [
     'CREATE_INSTANT_INVITE',
@@ -221,6 +227,7 @@ export const PERMISSIONS = [
     'READ_MESSAGE_HISTORY',
     'MENTION_EVERYONE',
     'USE_EXTERNAL_EMOJIS',
+    'VIEW_GUILD_ANALYTICS',
     'CONNECT',
     'SPEAK',
     'MUTE_MEMBERS',
@@ -233,41 +240,41 @@ export const PERMISSIONS = [
     'MANAGE_WEBHOOKS',
     'MANAGE_EMOJIS',
 ];
-
+   
 /**
- * @typedef {String} PERMISSIONS_NAMES
- * * CREATE_INSTANT_INVITE: 'Create Instant Invite'
- * * KICK_MEMBERS: 'Kick Members'
- * * BAN_MEMBERS: 'Ban Members'
- * * ADMINISTRATOR: 'Administrator',
- * *
- * * MANAGE_CHANNELS: 'Manage Channels'
- * * MANAGE_GUILD: 'Manage Guild'
- * * ADD_REACTIONS: 'Add Reactions'
- * * VIEW_AUDIT_LOG: 'View Audit Log'
- * * PRIORITY_SPEAKER: 'Priority Speaker'
- * * STREAM: 'Stream',
- * *
- * * VIEW_CHANNEL: 'Read Messages'
- * * SEND_MESSAGES: 'Send Messages'
- * * SEND_TTS_MESSAGES: 'Send TTS Messages'
- * * MANAGE_MESSAGES: 'Manage Messages'
- * * EMBED_LINKS: 'Embed Links'
- * * ATTACH_FILES: 'Attach Files'
- * * READ_MESSAGE_HISTORY: 'Read Message History'
- * * MENTION_EVERYONE: 'Mention Everyone'
- * * USE_EXTERNAL_EMOJIS: 'External Emojis'
- * * CONNECT: 'Voice Connect'
- * * SPEAK: 'Voice Speak'
- * * MUTE_MEMBERS: 'Voice Mute Members'
- * * DEAFEN_MEMBERS: 'Voice Deafen Members'
- * * MOVE_MEMBERS: 'Voice Move Members'
- * * USE_VAD: 'Voice Use VAD'
- * * CHANGE_NICKNAME: 'Change Nickname'
- * * MANAGE_NICKNAMES: 'Manage Nicknames'
- * * MANAGE_ROLES: 'Manage Roles'
- * * MANAGE_WEBHOOKS: 'Manage Webhooks'
- * * MANAGE_EMOJIS: 'Manage Emojis'
+ * @type {{
+ * CREATE_INSTANT_INVITE: 'Create Instant Invite',
+ * KICK_MEMBERS: 'Kick Members',
+ * BAN_MEMBERS: 'Ban Members',
+ * ADMINISTRATOR: 'Administrator',
+ * MANAGE_CHANNELS: 'Manage Channels',
+ * MANAGE_GUILD: 'Manage Guild',
+ * ADD_REACTIONS: 'Add Reactions',
+ * VIEW_AUDIT_LOG: 'View Audit Log',
+ * PRIORITY_SPEAKER: 'Priority Speaker',
+ * STREAM: 'Stream',
+ * VIEW_CHANNEL: 'Read Messages',
+ * SEND_MESSAGES: 'Send Messages',
+ * SEND_TTS_MESSAGES: 'Send TTS Messages',
+ * MANAGE_MESSAGES: 'Manage Messages',
+ * EMBED_LINKS: 'Embed Links',
+ * ATTACH_FILES: 'Attach Files',
+ * READ_MESSAGE_HISTORY: 'Read Message History',
+ * MENTION_EVERYONE: 'Mention Everyone',
+ * USE_EXTERNAL_EMOJIS: 'External Emojis',
+ * VIEW_GUILD_ANALYTICS: 'View Guild Analytics',
+ * CONNECT: 'Voice Connect',
+ * SPEAK: 'Voice Speak',
+ * MUTE_MEMBERS: 'Voice Mute Members',
+ * DEAFEN_MEMBERS: 'Voice Deafen Members',
+ * MOVE_MEMBERS: 'Voice Move Members',
+ * USE_VAD: 'Voice Use VAD',
+ * CHANGE_NICKNAME: 'Change Nickname',
+ * MANAGE_NICKNAMES: 'Manage Nicknames',
+ * MANAGE_ROLES: 'Manage Roles',
+ * MANAGE_WEBHOOKS: 'Manage Webhooks',
+ * MANAGE_EMOJIS: 'Manage Emojis',
+ * }}
  */
 export const PERMISSIONS_NAMES = {
     CREATE_INSTANT_INVITE: 'Create Instant Invite',
@@ -289,6 +296,7 @@ export const PERMISSIONS_NAMES = {
     READ_MESSAGE_HISTORY: 'Read Message History',
     MENTION_EVERYONE: 'Mention Everyone',
     USE_EXTERNAL_EMOJIS: 'External Emojis',
+    VIEW_GUILD_ANALYTICS: 'View Guild Analytics',
     CONNECT: 'Voice Connect',
     SPEAK: 'Voice Speak',
     MUTE_MEMBERS: 'Voice Mute Members',
--- src/Libraries/eris/lib/ErisEnums.js
+++ src/Libraries/eris/lib/ErisEnums.js
@@ -1,56 +1,58 @@
 /**
- * @typedef {String} EVENTS
- * * GUILD_CREATE: 'guildCreate'
- * * GUILD_DELETE: 'guildDelete'
- * * GUILD_UPDATE: 'guildUpdate'
- * * GUILD_UNAVAILABLE: 'guildUnavailable'
- * * GUILD_AVAILABLE: 'guildAvailable'
- * * GUILD_MEMBER_ADD: 'guildMemberAdd'
- * * GUILD_MEMBER_REMOVE: 'guildMemberRemove'
- * * GUILD_MEMBER_UPDATE: 'guildMemberUpdate'
- * * GUILD_MEMBER_AVAILABLE: 'guildMemberAvailable'
- * * GUILD_MEMBER_SPEAKING: 'guildMemberSpeaking'
- * * GUILD_MEMBERS_CHUNK: 'guildMembersChunk'
- * * GUILD_ROLE_CREATE: 'roleCreate'
- * * GUILD_ROLE_DELETE: 'roleDelete'
- * * GUILD_ROLE_UPDATE: 'roleUpdate'
- * * GUILD_EMOJIS_UPDATE: 'guildEmojisUpdate'
- * * GUILD_BAN_ADD: 'guildBanAdd'
- * * GUILD_BAN_REMOVE: 'guildBanRemove'
- * * UNAVAILABLE_GUILD_CREATE: 'unavailableGuildCreate'
- * * CHANNEL_CREATE: 'channelCreate'
- * * CHANNEL_DELETE: 'channelDelete'
- * * CHANNEL_UPDATE: 'channelUpdate'
- * * CHANNEL_PIN_UPDATE: 'channelPinUpdate'
- * * MESSAGE_CREATE: 'messageCreate'
- * * MESSAGE_DELETE: 'messageDelete'
- * * MESSAGE_UPDATE: 'messageUpdate'
- * * MESSAGE_DELETE_BULK: 'messageDeleteBulk'
- * * MESSAGE_REACTION_ADD: 'messageReactionAdd'
- * * MESSAGE_REACTION_REMOVE: 'messageReactionRemove'
- * * MESSAGE_REACTION_REMOVE_ALL: 'messageReactionRemoveAll'
- * * TYPING_START: 'typingStart'
- * * USER_UPDATE: 'userUpdate'
- * * PRESENCE_UPDATE: 'presenceUpdate'
- * * VOICE_CHANNEL_JOIN: 'voicecChannelJoin'
- * * VOICE_CHANNEL_LEAVE: 'voiceChannelLeave'
- * * VOICE_CHANNEL_SWITCH: 'voiceChannelSwitch'
- * * VOICE_STATE_UPDATE: 'voiceStateUpdate'
- * * WEBHOOKS_UPDATE: 'webhookUpdate'
- * * SHARD_DISCONNECT: 'shardDisconnect'
- * * SHARD_PRE_READY: 'shardPreReady'
- * * SHARD_READY: 'shardReady'
- * * SHARD_RESUME: 'shardResume'
- * * UNKNOWN: 'unknown'
- * * CONNECT: 'connect'
- * * DISCONNECT: 'disconnect'
- * * ERROR: 'error'
- * * WARN: 'warn'
- * * DEBUG: 'debug'
- * * READY: 'ready'
- * * HELLO: 'hello'
- * * RATE_LIMIT: 'rateLimit'
- * * RAW_WS: 'rawWS'
+ * @type {{
+ * GUILD_CREATE: 'guildCreate'
+ * GUILD_DELETE: 'guildDelete'
+ * GUILD_UPDATE: 'guildUpdate'
+ * GUILD_UNAVAILABLE: 'guildUnavailable'
+ * GUILD_AVAILABLE: 'guildAvailable'
+ * GUILD_MEMBER_ADD: 'guildMemberAdd'
+ * GUILD_MEMBER_REMOVE: 'guildMemberRemove'
+ * GUILD_MEMBER_UPDATE: 'guildMemberUpdate'
+ * GUILD_MEMBER_AVAILABLE: 'guildMemberAvailable'
+ * GUILD_MEMBER_SPEAKING: 'guildMemberSpeaking'
+ * GUILD_MEMBERS_CHUNK: 'guildMembersChunk'
+ * GUILD_ROLE_CREATE: 'roleCreate'
+ * GUILD_ROLE_DELETE: 'roleDelete'
+ * GUILD_ROLE_UPDATE: 'roleUpdate'
+ * GUILD_EMOJIS_UPDATE: 'guildEmojisUpdate'
+ * GUILD_BAN_ADD: 'guildBanAdd'
+ * GUILD_BAN_REMOVE: 'guildBanRemove'
+ * UNAVAILABLE_GUILD_CREATE: 'unavailableGuildCreate'
+ * CHANNEL_CREATE: 'channelCreate'
+ * CHANNEL_DELETE: 'channelDelete'
+ * CHANNEL_UPDATE: 'channelUpdate'
+ * CHANNEL_PIN_UPDATE: 'channelPinUpdate'
+ * MESSAGE_CREATE: 'messageCreate'
+ * MESSAGE_DELETE: 'messageDelete'
+ * MESSAGE_UPDATE: 'messageUpdate'
+ * MESSAGE_DELETE_BULK: 'messageDeleteBulk'
+ * MESSAGE_REACTION_ADD: 'messageReactionAdd'
+ * MESSAGE_REACTION_REMOVE: 'messageReactionRemove'
+ * MESSAGE_REACTION_REMOVE_ALL: 'messageReactionRemoveAll'
+ * TYPING_START: 'typingStart'
+ * USER_UPDATE: 'userUpdate'
+ * PRESENCE_UPDATE: 'presenceUpdate'
+ * VOICE_CHANNEL_JOIN: 'voicecChannelJoin'
+ * VOICE_CHANNEL_LEAVE: 'voiceChannelLeave'
+ * VOICE_CHANNEL_SWITCH: 'voiceChannelSwitch'
+ * VOICE_STATE_UPDATE: 'voiceStateUpdate'
+ * WEBHOOKS_UPDATE: 'webhookUpdate'
+ * SHARD_DISCONNECT: 'shardDisconnect'
+ * SHARD_PRE_READY: 'shardPreReady'
+ * SHARD_READY: 'shardReady'
+ * SHARD_RESUME: 'shardResume'
+ * UNKNOWN: 'unknown'
+ * CONNECT: 'connect'
+ * DISCONNECT: 'disconnect'
+ * ERROR: 'error'
+ * WARN: 'warn'
+ * DEBUG: 'debug'
+ * READY: 'ready'
+ * HELLO: 'hello'
+ * RATE_LIMIT: 'rateLimit'
+ * RAW_WS: 'rawWS'
+ * }}
+ * @enum {String}
  */
 export const EVENTS = {
     GUILD_CREATE: 'guildCreate',
@@ -107,37 +109,40 @@ export const EVENTS = {
 };
 
 /**
- * @typedef {String} DISCORD_LIB_PERMISSIONS
- * * CREATE_INSTANT_INVITE: 'createInstantInvite'
- * * KICK_MEMBERS: 'kickMembers'
- * * BAN_MEMBERS: 'banMembers'
- * * ADMINISTRATOR: 'administrator'
- * * MANAGE_CHANNELS: 'manageChannels'
- * * MANAGE_GUILD: 'manageGuild'
- * * ADD_REACTIONS: 'addReactions'
- * * VIEW_AUDIT_LOG: 'viewAuditLog'
- * * PRIORITY_SPEAKER: 'voicePrioritySpeaker'
- * * STREAM: 'stream'
- * * VIEW_CHANNEL: 'readMessages'
- * * SEND_MESSAGES: 'sendMessages'
- * * SEND_TTS_MESSAGES: 'sendTTSMessages'
- * * MANAGE_MESSAGES: 'manageMessages'
- * * EMBED_LINKS: 'embedLinks'
- * * ATTACH_FILES: 'attachFiles'
- * * READ_MESSAGE_HISTORY: 'readMessageHistory'
- * * MENTION_EVERYONE: 'mentionEveryone'
- * * USE_EXTERNAL_EMOJIS: 'externalEmojis'
- * * CONNECT: 'voiceConnect'
- * * SPEAK: 'voiceSpeak'
- * * MUTE_MEMBERS: 'voiceMuteMembers'
- * * DEAFEN_MEMBERS: 'voiceDeafenMembers'
- * * MOVE_MEMBERS: 'voiceMoveMembers'
- * * USE_VAD: 'voiceUseVAD'
- * * CHANGE_NICKNAME: 'changeNickname'
- * * MANAGE_NICKNAMES: 'manageNicknames'
- * * MANAGE_ROLES: 'manageRoles'
- * * MANAGE_WEBHOOKS: 'manageWebhooks'
- * * MANAGE_EMOJIS: 'manageEmojis'
+ * @type {{
+ * CREATE_INSTANT_INVITE: 'createInstantInvite'
+ * KICK_MEMBERS: 'kickMembers'
+ * BAN_MEMBERS: 'banMembers'
+ * ADMINISTRATOR: 'administrator'
+ * MANAGE_CHANNELS: 'manageChannels'
+ * MANAGE_GUILD: 'manageGuild'
+ * ADD_REACTIONS: 'addReactions'
+ * VIEW_AUDIT_LOG: 'viewAuditLog'
+ * PRIORITY_SPEAKER: 'voicePrioritySpeaker'
+ * STREAM: 'stream'
+ * VIEW_CHANNEL: 'readMessages'
+ * SEND_MESSAGES: 'sendMessages'
+ * SEND_TTS_MESSAGES: 'sendTTSMessages'
+ * MANAGE_MESSAGES: 'manageMessages'
+ * EMBED_LINKS: 'embedLinks'
+ * ATTACH_FILES: 'attachFiles'
+ * READ_MESSAGE_HISTORY: 'readMessageHistory'
+ * MENTION_EVERYONE: 'mentionEveryone'
+ * USE_EXTERNAL_EMOJIS: 'externalEmojis'
+ * VIEW_GUILD_ANALYTICS: 'viewGuildAnalytics'
+ * CONNECT: 'voiceConnect'
+ * SPEAK: 'voiceSpeak'
+ * MUTE_MEMBERS: 'voiceMuteMembers'
+ * DEAFEN_MEMBERS: 'voiceDeafenMembers'
+ * MOVE_MEMBERS: 'voiceMoveMembers'
+ * USE_VAD: 'voiceUseVAD'
+ * CHANGE_NICKNAME: 'changeNickname'
+ * MANAGE_NICKNAMES: 'manageNicknames'
+ * MANAGE_ROLES: 'manageRoles'
+ * MANAGE_WEBHOOKS: 'manageWebhooks'
+ * MANAGE_EMOJIS: 'manageEmojis'
+ * }}
+ * @enum {String}
  */
 export const DISCORD_LIB_PERMISSIONS = {
     CREATE_INSTANT_INVITE: 'createInstantInvite',
@@ -159,6 +164,7 @@ export const DISCORD_LIB_PERMISSIONS = {
     READ_MESSAGE_HISTORY: 'readMessageHistory',
     MENTION_EVERYONE: 'mentionEveryone',
     USE_EXTERNAL_EMOJIS: 'externalEmojis',
+    VIEW_GUILD_ANALYTICS: 'viewGuildAnalytics',
     CONNECT: 'voiceConnect',
     SPEAK: 'voiceSpeak',
     MUTE_MEMBERS: 'voiceMuteMembers',
@@ -173,37 +179,39 @@ export const DISCORD_LIB_PERMISSIONS = {
 };
 
 /**
- * @typedef {Array<String>} PERMISSIONS
- * * 'createInstantInvite'
- * * 'kickMembers'
- * * 'banMembers'
- * * 'administrator'
- * * 'manageChannels'
- * * 'manageGuild'
- * * 'addReactions'
- * * 'viewAuditLog'
- * * 'voicePrioritySpeaker'
- * * 'stream'
- * * 'readMessages'
- * * 'sendMessages'
- * * 'sendTTSMessages'
- * * 'manageMessages'
- * * 'embedLinks'
- * * 'attachFiles'
- * * 'readMessageHistory'
- * * 'mentionEveryone'
- * * 'externalEmojis'
- * * 'voiceConnect'
- * * 'voiceSpeak'
- * * 'voiceMuteMembers'
- * * 'voiceDeafenMembers'
- * * 'voiceMoveMembers'
- * * 'voiceUseVAD'
- * * 'changeNickname'
- * * 'manageNicknames'
- * * 'manageRoles'
- * * 'manageWebhooks'
- * * 'manageEmojis'
+ * @type {[
+    'createInstantInvite',
+    'kickMembers',
+    'banMembers',
+    'administrator',
+    'manageChannels',
+    'manageGuild',
+    'addReactions',
+    'viewAuditLog',
+    'voicePrioritySpeaker',
+    'stream',
+    'readMessages',
+    'sendMessages',
+    'sendTTSMessages',
+    'manageMessages',
+    'embedLinks',
+    'attachFiles',
+    'readMessageHistory',
+    'mentionEveryone',
+    'externalEmojis',
+    'viewGuildAnalytics',
+    'voiceConnect',
+    'voiceSpeak',
+    'voiceMuteMembers',
+    'voiceDeafenMembers',
+    'voiceMoveMembers',
+    'voiceUseVAD',
+    'changeNickname',
+    'manageNicknames',
+    'manageRoles',
+    'manageWebhooks',
+    'manageEmojis',
+ * ]}
  */
 export const PERMISSIONS = [
     'createInstantInvite',
@@ -225,6 +233,7 @@ export const PERMISSIONS = [
     'readMessageHistory',
     'mentionEveryone',
     'externalEmojis',
+    'viewGuildAnalytics',
     'voiceConnect',
     'voiceSpeak',
     'voiceMuteMembers',
@@ -239,38 +248,41 @@ export const PERMISSIONS = [
 ];
 
 /**
- * @typedef {String} PERMISSIONS_NAMES
- * * createInstantInvite: 'Create Instant Invite'
- * * kickMembers: 'Kick Members'
- * * banMembers: 'Ban Members'
- * * administrator: 'Administrator'
- * * manageChannels: 'Manage Channels'
- * * manageGuild: 'Manage Guild'
- * * addReactions: 'Add Reactions'
- * * viewAuditLog: 'View Audit Log'
- * * voicePrioritySpeaker: 'Priority Speaker'
- * * stream: 'Stream'
- * * readMessages: 'Read Messages'
- * * sendMessages: 'Send Messages'
- * * sendTTSMessages: 'Send TTS Messages'
- * * manageMessages: 'Manage Messages'
- * * embedLinks: 'Embed Links'
- * * attachFiles: 'Attach Files'
- * * readMessageHistory: 'Read Message History'
- * * mentionEveryone: 'Mention Everyone'
- * * externalEmojis: 'External Emojis'
- * * voiceConnect: 'Voice Connect'
- * * voiceSpeak: 'Voice Speak'
- * * voiceMuteMembers: 'Voice Mute Members'
- * * voiceDeafenMembers: 'Voice Deafen Members'
- * * voiceMoveMembers: 'Voice Move Members'
- * * voiceUseVAD: 'Voice Use VAD'
- * * changeNickname: 'Change Nickname'
- * * manageNicknames: 'Manage Nicknames'
- * * manageRoles: 'Manage Roles'
- * * manageWebhooks: 'Manage Webhooks'
- * * manageEmojis: 'Manage Emojis'
-*/
+ * @type {{
+ * createInstantInvite: 'Create Instant Invite'
+ * kickMembers: 'Kick Members'
+ * banMembers: 'Ban Members'
+ * administrator: 'Administrator'
+ * manageChannels: 'Manage Channels'
+ * manageGuild: 'Manage Guild'
+ * addReactions: 'Add Reactions'
+ * viewAuditLog: 'View Audit Log'
+ * voicePrioritySpeaker: 'Priority Speaker'
+ * stream: 'Stream'
+ * readMessages: 'Read Messages'
+ * sendMessages: 'Send Messages'
+ * sendTTSMessages: 'Send TTS Messages'
+ * manageMessages: 'Manage Messages'
+ * embedLinks: 'Embed Links'
+ * attachFiles: 'Attach Files'
+ * readMessageHistory: 'Read Message History'
+ * mentionEveryone: 'Mention Everyone'
+ * externalEmojis: 'External Emojis'
+ * viewGuildAnalytics: 'View Guild Analytics'
+ * voiceConnect: 'Voice Connect'
+ * voiceSpeak: 'Voice Speak'
+ * voiceMuteMembers: 'Voice Mute Members'
+ * voiceDeafenMembers: 'Voice Deafen Members'
+ * voiceMoveMembers: 'Voice Move Members'
+ * voiceUseVAD: 'Voice Use VAD'
+ * changeNickname: 'Change Nickname'
+ * manageNicknames: 'Manage Nicknames'
+ * manageRoles: 'Manage Roles'
+ * manageWebhooks: 'Manage Webhooks'
+ * manageEmojis: 'Manage Emojis'
+ * }}
+ * @enum {String}
+ */
 export const PERMISSIONS_NAMES = {
     createInstantInvite: 'Create Instant Invite',
     kickMembers: 'Kick Members',
@@ -291,6 +303,7 @@ export const PERMISSIONS_NAMES = {
     readMessageHistory: 'Read Message History',
     mentionEveryone: 'Mention Everyone',
     externalEmojis: 'External Emojis',
+    viewGuildAnalytics: 'View Guild Analytics',
     voiceConnect: 'Voice Connect',
     voiceSpeak: 'Voice Speak',
     voiceMuteMembers: 'Voice Mute Members',
