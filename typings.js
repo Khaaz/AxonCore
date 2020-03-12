a38c36a7e1b66bfb1a4f1c11061989f43ec54043
Bsian <bsian@staff.libraryofcode.us>
2020-03-12 21:10:37 +0000

    Remove language check/selection

--- typings.js
+++ typings.js
@@ -1,16 +1,9 @@
 const { prompt } = require('inquirer');
-const fs = require('fs');
+const { copyFileSync, writeFileSync } = require('fs');
 let config;
 
 async function promptTypings() {
     const answer = await prompt( [
-        {
-            name: 'usesTS',
-            type: 'list',
-            message: 'Does your AxonCore project use TypeScript or JavaScript?',
-            choices: ['JavaScript', 'TypeScript'],
-            filter: (input) => input === 'TypeScript',
-        },
         {
             name: 'library',
             type: 'list',
@@ -24,7 +17,6 @@ async function promptTypings() {
                 },
             ],
             filter: (input) => input.toLowerCase().replace('.', ''),
-            when: (answers) => answers.usesTS,
         },
     ] );
     config = answer;
@@ -39,9 +31,6 @@ const libraries = [
 async function checkConfig() {
     try {
         config = require('../../.axoncorerc.json');
-        if (!config.usesTS && config.usesTS !== false) {
-            throw new Error();
-        }
         
         if (!libraries.includes(config.library) ) {
             throw new Error();
@@ -51,19 +40,15 @@ async function checkConfig() {
         await promptTypings();
     }
 
-    if (!config.usesTS) {
-        console.log('AxonCore project does not use TypeScript. Documentation installation complete.');
-        process.exit(0);
-    }
     if (config.library === 'detritus') {
         console.warn('Detritus support for AxonCore is not yet available. Please consider using another library, or keep a look out on the AxonCore repository.');
         process.exit(1);
     }
 
     console.log(`Copying documentation for the ${config.library} library...`);
-    fs.copyFileSync(`./types/Libraries/${config.library}.ts`, `./types/Libraries/index.ts`);
+    copyFileSync(`./types/Libraries/${config.library}.ts`, `./types/Libraries/index.ts`);
     console.log(`Library documentation copying complete. Saving configuration file...`);
-    fs.writeFileSync('../../.axoncorerc.json', JSON.stringify(config) );
+    writeFileSync('../../.axoncorerc.json', JSON.stringify(config) );
 }
 
 console.log('Thank you for installing AxonCore.');
