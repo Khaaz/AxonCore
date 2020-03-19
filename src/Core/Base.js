bab1c1ccf55bc2a6721569665be60d7acefb7e72
KhaaZ <khaaz.dev@gmail.com>
2020-03-19 13:29:51 +0100

    Update src/Core/Base.js
    Co-Authored-By: bsian03 <bsian@staff.libraryofcode.us>

--- src/Core/Base.js
+++ src/Core/Base.js
@@ -189,7 +189,7 @@ class Base {
      * @param {String|MessageObject} content - Message content, String or Embed Object
      * @param {Object} [options={}] - Options { disableEveryone: Boolean, delete: Boolean, delay: Number }
      * @param {Boolean} [options.disableEveryone=true] - Whether to allow mentioning everyone or not
-     * @param {Object} [options.allowedMentions] - Cutom allowed mentions object
+     * @param {Object} [options.allowedMentions] - Custom allowed mentions object
      * @param {Boolean} [options.delete=false] - Whether to delete the message or not
      * @param {Number} [options.delay=null] - Delay after which the message will be deleted
      * @returns {Promise<Message?>} Message Object
