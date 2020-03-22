7980dc2e2aedd7ff178faf3b05fe90004fffbb28
Khaazz <khaaz.dev@gmail.com>
2020-03-22 19:03:05 +0100

    fix(detritus): hasPermission

--- src/Libraries/detritusjs/lib/DetritusChannel.js
+++ src/Libraries/detritusjs/lib/DetritusChannel.js
@@ -2,6 +2,12 @@ import Channel from '../../definitions/Channel';
 
 class DetritusChannel extends Channel {
     hasPermission(channel, user, perm) {
+        if (!user.permissions && channel.guild) {
+            user = channel.guild.members.get(user.id);
+        } else {
+            return true;
+        }
+
         return channel.can(perm, user);
     }
     
