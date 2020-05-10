8d6d4dad57abdff2e4ed7d1871de0f58c4b96851
Bsian <bsian@staff.libraryofcode.us>
2020-05-10 20:37:10 +0100

    For some reason this didn't get included

--- types/Database/MongoProvider.ts
+++ types/Database/MongoProvider.ts
@@ -63,7 +63,7 @@ export declare class MongoProvider extends ADBProvider {
      * @returns GuildDocument or null
      * @memberof MongoProvider
      */
-    fetchGuildSchema(gID: string): Promise<GuildDocument | null>;
+    fetchGuildDocument(gID: string): Promise<GuildDocument | null>;
 
     /**
      * Update AxonConfig in the DB.
