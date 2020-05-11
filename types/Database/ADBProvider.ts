2f86941ba671e1a5bec862c3948525f75493b5f4
Bsian <bsian@staff.libraryofcode.us>
2020-05-11 23:12:45 +0100

    Add typings for previous commit

--- types/Database/ADBProvider.ts
+++ types/Database/ADBProvider.ts
@@ -114,4 +114,7 @@ export declare abstract class ADBProvider {
      * @memberof ADBProvider
      */
     public saveGuild(gID: string, data: GuildConfig): Promise<GuildConfig | null>; // Not Implemented
+
+    public AxonConfig: new (...args: any) => AxonConfig;
+    public GuildConfig: new (...args: any) => GuildConfig;
 }
