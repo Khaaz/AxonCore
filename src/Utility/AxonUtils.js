abeda178429d91250a6f17d6c90ae6be4a1a6a86
Null <null@voidnull.dev>
2020-07-25 18:38:02 -0500

    fix(AxonUtils): isBotStaff shouldn't error now

--- src/Utility/AxonUtils.js
+++ src/Utility/AxonUtils.js
@@ -172,8 +172,8 @@ class AxonUtils {
      * @memberof AxonUtils
      */
     isBotStaff(uID) {
-        for (const rank in this.axon.staff) {
-            if (rank.find(u => u === uID) ) {
+        for (const rank in Object.values(this.axon.staff) ) {
+            if (rank.includes(uID) ) {
                 return true;
             }
         }
