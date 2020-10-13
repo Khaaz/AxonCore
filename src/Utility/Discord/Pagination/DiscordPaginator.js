b956a18fc11a4bb4b67899514c9ffb5ec1ba5281
Bavfalcon9 <olybear9@gmail.com>
2020-10-13 15:10:08 -0500

    feat(paginator): Add page navigation

---
+++ src/Utility/Discord/Pagination/DiscordPaginator.js
@@ -0,0 +1,18 @@
+import NoAbstractInstanceException from '../../../Errors/NoAbstractInstanceException';
+
+/**
+ * To Do:
+ * - you give them a collector (reaction or message: you can wait fo rmy update on collectors that will makes then slightly easier to use)
+ * - you give them a list of page to render as discord embeds
+ * - you need to be able to have one instance of the paginator per list of pages (aka not having to recreate at every command run) that you call several time to send the pages (I think? but not sure about this one)
+ * - you need to be able to call the paginator with just the page number for both react and message and it shows the called page. Then it acts as a regular paginator.
+ * - There should be a different way to call the paginator (maybe can be when you create a paginator and not a MessagePaginator / ReactionPaginator but not sure about that). The only thing I want is that when you call this Paginator with a different method, you get the correct page rendered but it doesn't await anything, doesn't put reaction or anything
+ **/
+class DiscordPaginator {
+     constructor() {
+          if (this.constructor.name === 'DiscordPaginator') {
+               throw new NoAbstractInstanceException();
+          }
+     }
+}
+export default DiscordPaginator;
--- src/Utility/Discord/Pagination/Paginator.js
+++ src/Utility/Discord/Pagination/Paginator.js
@@ -1,32 +1,72 @@
 import Chunker from './Chunker';
 
-/**
- * To Do:
- * - ~~chunker that directly handle the data to output~~
- * - renderer that display the data needed like we want
- * - navigator that will take user input and call the rendered to correctly render the data
- **/
 class Paginator {
      /** @var {Chunker} chunker */
      #chunker;
-     /** @var {Function<any[]>} */
-     #renderFunc;
 
-     constructor(data = [], pages = 2, closure) {
+     /** @var {number} page */
+     #page;
+
+     constructor(data = [], pages = 2) {
           this.#chunker = new Chunker(data);
           this.#chunker.chunk(pages);
-          this.#renderFunc = closure;
+          this.#page = 0;
      }
 
      /**
-      * Unsure what to do here @Khaaz
+      * @todo Should we implement pages where it will index at 0 if a given page is invalid.
       */
-     render(data) {
-          return this.#renderFunc(data);
+     getPreviousPage() {
+          if (!this.pages[--this.#page]) {
+               throw new RangeError('Page out of bounds');
+          }
+          return this.pages[this.#page];
      }
 
+     /**
+      * @todo Should we implement pages where it will index at 0 if a given page is invalid.
+      */
+     getNextPage() {
+          if (!this.pages[++this.#page]) {
+               throw new RangeError('Page out of bounds');
+          }
+          return this.pages[this.#page];
+     }
+
+     /**
+      * Gets a specific page in the chunked data
+      * @param {number} number 
+      */
+     getPage(number = null) {
+          this.#page = number || this.#page;
+
+          if (!this.pages[this.#page]) {
+               this.#page = 0;
+          }
+
+          return this.pages[this.#page];
+     }
+
+     /**
+      * Adds a page to the paginator
+      * @param {any} data - Data to add
+      */
+     addPage(data) {
+          return this.#chunker.add(data);
+     }
+
+     /**
+      * Gets all the pages as an array
+      */
      get pages() {
           return this.#chunker.chunks;
+     }
+
+     /**
+      * Gets the data 
+      */
+     get data() {
+          return this.#chunker.data;
      }
 }
 export default Paginator;
