3f0b7c0e64a425fd7efc2025d012f03f2f3077ae
Khaaz <khaaz.dev@gmail.com>
2020-10-18 12:17:13 +0200

    fix(djs/RawHandler): payload.d can be not present

--- src/Libraries/discordjs/handlers/RawHandler.js
+++ src/Libraries/discordjs/handlers/RawHandler.js
@@ -2,11 +2,15 @@ import AHandler from 'axoncore/src/Core/Event/AHandler';
 
 class RawHandler extends AHandler {
     handle(payload) {
-        return payload.d.guild_id
-            ? payload.d.guild_id
-            : (payload.d.guild
+        if (payload.d) {
+            if (payload.d.guild_id) {
+                return payload.d.guild_id;
+            }
+            return payload.d.guild
                 ? payload.d.guild.id
-                : null);
+                : null;
+        }
+        return null;
     }
 }
 
