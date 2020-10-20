ca749a6f575a358e71aa1fb61cfb5f17c9f0fe67
Bsian <bsian@staff.libraryofcode.us>
2020-10-20 07:50:49 +0100

    Working system

--- src/AxonClient.js
+++ src/AxonClient.js
@@ -38,21 +38,36 @@ import { WEBHOOK_TYPES, LOG_LEVELS, WEBHOOK_TO_COLOR, DEBUG_FLAGS } from './Util
 function createMessageManagerProxy(_messageManager) {
     const handler = {
         get(t, name) {
-            if (name === undefined || typeof prop === 'symbol') {
-                return t;
-            }
             const path = [];
-            const target = (args, lang) => _messageManager.get(path.join('.'), args, lang);
+            const MessageManagerProxy = (args, lang) => {
+                console.log(path);
+                /*
+                if (path[path.length - 1] === 'then' && typeof args === 'function' && typeof lang === 'function') {
+                    console.log('ASYNC_AWAIT_EXECUTION_DETECTED');
+                    console.log(handlerLoop);
+                    return Promise.resolve(new Proxy(MessageManagerProxy, handlerLoop)); // Async special case
+                }
+                */
+                if ( (args !== undefined && typeof args !== 'object') || (lang !== undefined && typeof lang !== 'string') ) {
+                    throw new TypeError('INVALID_TYPE_IN_PARAMETER');
+                }
+                return path.length ? _messageManager.get(path.join('.'), args, lang) : _messageManager;
+            };
+            
+            if (name === undefined || typeof name === 'symbol' || (!path.length && name === 'then') ) {
+                return t; // MessageManager
+            }
 
             const handlerLoop = {
                 get(obj, prop) {
+                    console.log('handlerLoop prop: ', prop); console.log('path: ', path);
                     if (prop === undefined || typeof prop === 'symbol') {
                         return obj;
                     }
-                    return prop in obj ? obj[prop] : (path.push(prop) && new Proxy(target, handlerLoop) );
+                    return prop in obj ? obj[prop] : (path.push(prop) && new Proxy(MessageManagerProxy, handlerLoop) );
                 },
             };
-            return name in t ? t[name] : (path.push(name) && new Proxy(target, handlerLoop) );
+            return name in t ? t[name] : (path.push(name) && new Proxy(MessageManagerProxy, handlerLoop) );
         },
     };
     return new Proxy(_messageManager, handler);
